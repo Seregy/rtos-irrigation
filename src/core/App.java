e9570632b512616426bdfe78f39ccdbab3ede153
Seregy <seregy77@gmail.com>
2017-11-08 01:35:49 +0200

    Add ability to read commands from file

--- src/core/App.java
+++ src/core/App.java
@@ -1,7 +1,40 @@
 package core;
 
+import command.Command;
+import parser.Lexer;
+import parser.TokenParser;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path of the file to be parsed:");
+        String path = scanner.nextLine();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-16"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = reader.readLine();
 
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = reader.readLine();
+            }
+
+            String result = sb.toString();
+            List<Command> commands = new TokenParser(new Lexer()).parse(result);
+            for (Command command : commands) {
+                System.out.println(command.getName());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }

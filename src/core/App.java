deb3f1bc6711ecb54c805d50c876b8289bf489da
sergfrolov <sergey.frolov.main@gmail.com>
2017-12-19 20:05:14 +0200

    wtf is wrong
    wtf is wrong

--- src/core/App.java
+++ src/core/App.java
@@ -193,7 +193,6 @@ public class App extends Application{
             if (zone.getWateringStatus() != WateringStatus.DISABLED) {
                 continue;
             }
-
             zone.setWateringStatus(WateringStatus.ENABLED);
             zoneDAO.update(zone);
             setWateringTimersForZone(zone);
@@ -344,6 +343,11 @@ public class App extends Application{
         Zone zone = zoneDAO.find(7);
         zone.setHumidityValue(50);
         resetZoneState(7);
+    }
+
+    public void resumeWateringButton(){
+       int[] zones = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+       resumeWatering(new ResumeWatering(zones));
     }
 
     private void resetZoneState(int id) {

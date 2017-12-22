6753ab658b28304d6b20a5e8b4d9f041d0a0f154
Seregy77 <Seregy77@gmail.com>
2017-12-22 12:29:54 +0200

    Add new tests for commands, fix existing tests
    - use new App's constructor for mocking MainWindow
    - use junit's assert methods instead of mockito's verify where possible

--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import parser.Parser;
 import ui.MainWindow;
+import zone.FertilizingStatus;
+import zone.WateringStatus;
 import zone.Zone;
 import zone.ZoneDAO;
 
@@ -28,12 +30,10 @@ public class AppTest {
                 new AbstractMap.SimpleImmutableEntry<>(30, 40)));
         when(mockedParser.parse(input)).thenReturn(commands);
 
-        App app = new App(mockedDAO, mockedParser);
-        app.setMainWindowController(mock(MainWindow.class));
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
         app.handleCommands(input);
 
-        verify(mockedDAO, times(6)).find(anyInt());
-        verify(mockedDAO, times(6)).update(any());
+        Assertions.assertNotEquals(WateringStatus.NOT_INITIALISED, mockedDAO.find(7).getWateringStatus());
     }
 
     @Test
@@ -52,13 +52,12 @@ public class AppTest {
                 new ResumeWatering(new int[] {7, 12}));
         when(mockedParser.parse(input)).thenReturn(commands);
 
-        App app = new App(mockedDAO, mockedParser);
-        app.setMainWindowController(mock(MainWindow.class));
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
         app.handleCommands(input);
 
-        verify(mockedDAO, times(13)).find(anyInt());
-        verify(mockedDAO, times(8)).update(any());
+        Assertions.assertNotEquals(WateringStatus.NOT_INITIALISED, mockedDAO.find(7).getWateringStatus());
     }
+
 
     @Test
     void throwErrorOnParserError() throws ParseException {
@@ -67,12 +66,183 @@ public class AppTest {
 
         when(mockedParser.parse(anyString())).thenThrow(ParseException.class);
 
-        App app = new App(mockedDAO, mockedParser);
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
         Assertions.assertThrows(ParseException.class,
                 () -> app.handleCommands("ПідключитиПолив: (7-10, 12, 15), " +
                         "2017-11-01 10:10, 00:30, 1, 2, 30-40;"));
 
-        verify(mockedDAO, times(0)).find(anyInt());
+        verify(mockedDAO, times(0)).update(any());
+    }
+
+    @Test
+    void handleChangeWatering() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиПолив: (7-10, 12, 15), 2017-11-01 10:10, 00:30, 1, 2, 30-40;" +
+                "ЗмінитиПолив: 10, 2017-12-21 12:00, 00:15, 2, 3, 10-20;";
+
+        LocalTime interval = LocalTime.of(0, 15);
+        List<Command> commands = Arrays.asList(new EnableWatering(new int[]{7, 8, 9, 10, 12, 15},
+                LocalDateTime.of(2017, 11, 1, 10, 10),
+                LocalTime.of(0, 30), 1, 2,
+                new AbstractMap.SimpleImmutableEntry<>(30, 40)),
+                new ChangeWatering(new int[] {10},
+                        LocalDateTime.of(2017, 12, 21, 12, 0),
+                        interval, 2, 3.0,
+                        new AbstractMap.SimpleImmutableEntry<>(10, 20)));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+
+        Assertions.assertEquals(interval, mockedDAO.find(10).getWateringInterval());
+    }
+
+
+    @Test
+    void handleChangeWateringWithoutOneParam() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиПолив: (7-10, 12, 15), , 00:30, 1, 2, 30-40;" +
+                "ЗмінитиПолив: 10, 2017-12-21 12:00, 00:15, 2, 3, 10-20;";
+
+        LocalDateTime date = LocalDateTime.of(2017, 11, 1, 10, 10);
+        List<Command> commands = Arrays.asList(new EnableWatering(new int[]{7, 8, 9, 10, 12, 15},
+                        date,
+                        LocalTime.of(0, 30), 1, 2,
+                        new AbstractMap.SimpleImmutableEntry<>(30, 40)),
+                new ChangeWatering(new int[] {10},
+                        null,
+                        LocalTime.of(0, 30), 2, 3.0,
+                        new AbstractMap.SimpleImmutableEntry<>(10, 20)));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+
+        Assertions.assertEquals(date, mockedDAO.find(10).getFirstWatering());
+    }
+
+    @Test
+    void handleChangeWateringWithoutTwoParam() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиПолив: (7-10, 12, 15), , 00:30, 1, 2, 30-40;" +
+                "ЗмінитиПолив: 10, 2017-12-21 12:00, 00:15, 2, 3, 10-20;";
+
+        LocalDateTime date = LocalDateTime.of(2017, 11, 1, 10, 10);
+        LocalTime time = LocalTime.of(0, 1);
+        List<Command> commands = Arrays.asList(new EnableWatering(new int[]{7, 8, 9, 10, 12, 15},
+                        date, time, 1, 2,
+                        new AbstractMap.SimpleImmutableEntry<>(30, 40)),
+                new ChangeWatering(new int[] {10},
+                        null,
+                        null, 2, 3.0,
+                        new AbstractMap.SimpleImmutableEntry<>(10, 20)));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+
+        Assertions.assertEquals(date, mockedDAO.find(10).getFirstWatering());
+        Assertions.assertEquals(time, mockedDAO.find(10).getWateringInterval());
+    }
+
+    @Test
+    void handleSetSensorPeriodicityCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ЗадатиПеріодичністьДатчиків: 8, 00:03;";
+        LocalTime periodicity = LocalTime.of(0, 3);
+        List<Command> commands = Collections.singletonList(new SetSensorPeriodicity(new int[]{8}, periodicity));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+
+        Assertions.assertEquals(periodicity, mockedDAO.find(8).getSensorsCheckInterval());
+    }
+
+    @Test
+    void handleShowHumidityCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиПолив: 1, 2017-11-01 10:10, 00:30, 1, 2, 30-40; ПоказатиРівеньВологості: (1, 2);";
+        List<Command> commands = Arrays.asList(new EnableWatering(new int[]{1},
+                LocalDateTime.of(2017, 11, 1, 10, 10),
+                LocalTime.of(0, 30), 1, 2,
+                new AbstractMap.SimpleImmutableEntry<>(30, 40)),
+                new ShowHumidity(new int[]{1, 2}));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        MainWindow mockedWindow = mock(MainWindow.class);
+        App app = new App(mockedDAO, mockedParser, mockedWindow);
+        app.handleCommands(input);
+        verify(mockedDAO, times(3)).update(mockedDAO.find(1));
+        verify(mockedWindow, atLeastOnce()).print(anyString());
+    }
+
+    @Test
+    void handleEnableFertilizingCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиУдобрювання: 1, 5;";
+        List<Command> commands = Collections.singletonList(new EnableFertilizing(new int[]{1}, 5));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+        Assertions.assertEquals(5, mockedDAO.find(1).getFertilizerVolume());
+    }
+
+    @Test
+    void handleShowFertilizingCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПоказатиУдобрювання: 1;";
+        List<Command> commands = Collections.singletonList(new ShowFertilizing(new int[]{1}));
+        when(mockedParser.parse(input)).thenReturn(commands);
+        MainWindow mockedWindow = mock(MainWindow.class);
+        App app = new App(mockedDAO, mockedParser, mockedWindow);
+        app.handleCommands(input);
+        verify(mockedWindow, atLeastOnce()).print(anyString());
+    }
+
+    @Test
+    void handleChangeFertilizingCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ЗмінитиУдобрювання: 1, 12;";
+        List<Command> commands = Collections.singletonList(new ChangeFertilizing(new int[]{1}, 12));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+        Assertions.assertEquals(12, mockedDAO.find(1).getFertilizerVolume());
+    }
+
+    @Test
+    void handleStopFertilizingCommand() throws ParseException{
+        ZoneDAO mockedDAO = mockZoneDAO();
+        Parser mockedParser = mock(Parser.class);
+
+        String input = "ПідключитиУдобрювання: 1, 2; ЗупинитиУдобрювання: (1, 2);";
+        List<Command> commands = Arrays.asList(new EnableFertilizing(new int[] {1}, 2),
+                new StopFertilizing(new int[]{1, 2}));
+        when(mockedParser.parse(input)).thenReturn(commands);
+
+        App app = new App(mockedDAO, mockedParser, mock(MainWindow.class));
+        app.handleCommands(input);
+        Assertions.assertEquals(FertilizingStatus.DISABLED, mockedDAO.find(1).getFertilizingStatus());
+        Assertions.assertEquals(FertilizingStatus.NOT_INITIALISED, mockedDAO.find(2).getFertilizingStatus());
     }
 
     private ZoneDAO mockZoneDAO() {
